7f5068ea4469e57febb93ede613f1212c212fd4d
Storm O'Bryant <vmarie1997@gmail.com>
2022-10-24 23:09:17 -0500

    completed exercises

---
+++ CollectionExercies/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CollectionExercises
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // EXERCISE ONE
+      // array of nums between 0-9
+      int[] numArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      // array of names tim, martin, nikki, & sara
+      string[] nameArr = { "Tim", "Martin", "Nikki", "Sara" };
+      // array of true/false values, length of 10, starting with true
+      bool[] boolArr = { true, false, true, false, true, false, true, false, true, false };
+
+      
+      // EXERCISE TWO
+      //Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!) 
+      List<string> iceCreamFlavors = new List<string>();
+      iceCreamFlavors.Add("Rocky Road");
+      iceCreamFlavors.Add("Pralines & Cream");
+      iceCreamFlavors.Add("S'mores");
+      iceCreamFlavors.Add("Cookie Crisp");
+      iceCreamFlavors.Add("Banana Nut");
+      iceCreamFlavors.Add("Spongebob SpongePop");
+      iceCreamFlavors.Add("Cocoa Crunch");
+      //Output the length of this list after building it 
+      Console.WriteLine($"Length of iceCreamFlavors is: {iceCreamFlavors.Count}");
+      //Output the third flavor in the list,
+      Console.WriteLine($"Third flavor in the list is: {iceCreamFlavors[2]}");
+      // then remove this value
+      iceCreamFlavors.RemoveAt(2);
+      // Output the new length of the list (It should just be one fewer!)
+      Console.WriteLine($"iceCreamFlavors after removing 3rd item: {iceCreamFlavors.Count}\n\n");
+
+
+      // EXERCISE THREE
+      //Create a dictionary that will store both string keys as well as string values
+      Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+      // Add key/value pairs to this dictionary where: -each key is a name from your names array -each value is a randomly select a flavor from your flavors list. 
+      Random random = new Random();
+      myDictionary.Add(nameArr[0], iceCreamFlavors[random.Next(0, 7)]);
+      myDictionary.Add(nameArr[1], iceCreamFlavors[random.Next(0, 7)]);
+      myDictionary.Add(nameArr[2], iceCreamFlavors[random.Next(0, 7)]);
+      myDictionary.Add(nameArr[3], iceCreamFlavors[random.Next(0, 7)]);
+      //Loop through the dictionary and print out each user's name and their associated ice cream flavor
+      foreach(var item in myDictionary)
+        Console.WriteLine($"{item.Key} likes {item.Value} ice cream");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Collections_Assignment_CSHARP
-Collections assignment and Codealong solution 
 
-### Read for Better Understand of:
+## 10/25/22-Storm O'Bryant
+
+Collections assignment and Codealong solution
+
+### Read for Better Understand of
+
 - [Lists](https://centria.github.io/basic-coding/part3/2/)
 - [Arrays](https://centria.github.io/basic-coding/part3/3/)
 - [Dictionaries](https://centria.github.io/basic-coding/part8/1/)
@@ -11,25 +15,23 @@ Collections assignment and Codealong solution
 - [Using and Namespaces](https://centria.github.io/basic-coding/part12/1/)
 - I
 
-
-### The Collection Exercises are below:
+### The Collection Exercises are below
 
 NOTE: Do the best you can to complete all three! Comment your steps to show how you approach problem solving
 
-1. Three Basic Arrays.
+1.Three Basic Arrays.
 
 Create an array to hold integer values 0 through 9 Create an array of the names "Tim", "Martin", "Nikki", & "Sara" Create an array of length 10 that alternates between true and false values, starting with true
 
-2. List of Flavors.
+2.List of Flavors.
 
 Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!) Output the length of this list after building it Output the third flavor in the list, then remove this value Output the new length of the list (It should just be one fewer!)
 
-3. User Info Dictionary.
+3.User Info Dictionary.
 
 Create a dictionary that will store both string keys as well as string values Add key/value pairs to this dictionary where: -each key is a name from your names array -each value is a randomly select a flavor from your flavors list. Loop through the dictionary and print out each user's name and their associated ice cream flavor
 
-
-## Collection references are below:
+## Collection references are below
 
 - [Interfaces vs inheritance](https://www.codecademy.com/learn/build-web-apps-asp-net-intermediate-c-sharp/modules/learn-csharp-interfaces-inheritance/cheatsheet)
 - [C# types of collections](https://zetcode.com/csharp/collection/)
